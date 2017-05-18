9840df023770ca9a7f0ead5a6a5d877844c04f3e
Millsy55 <adam.millsy@gmail.com>
2017-05-18 12:12:40 -0400

    More Starting Code
    Not even close to a working version

--- popup.js
+++ popup.js
@@ -4,11 +4,13 @@
 document.addEventListener("click", sine);
 var canvas = document.getElementById("myCanvas");
 var dt = canvas.getContext('2d');
+var freq = 1000, freqMultiplier = 1;
 dt.moveTo(10,10);
 dt.lineTo(50,50);
 dt.stroke();
-var aud = document.getElementById('SineWave.mp3');
+var sineWave = document.getElementById('SineWave.mp3');
 function sine(){
 	dt.fillText("click", event.clientX, event.clientY);
-	aud.play();
+	sineWave.playbackRate = freqMultiplier;
+	sineWave.play();
 }
